50408cf10fba3b13e44038ae49ebf4615c4ffd87
Sophia Jade Esclares <sophiajade.esclares@g.msuiit.edu.ph>
2023-07-01 06:16:07 +0800

    manifesting

--- dist/js/user.js
+++ dist/js/user.js
@@ -17,6 +17,20 @@ options.forEach(option =>{
     })
 })
 
+//deactivation
+const section = document.querySelector("section"),
+        overlay = document.querySelector(".overlay"),
+        showBtn = document.querySelector(".show-modal"),
+        closeBtn = document.querySelector(".close-btn");
+        
+      showBtn.addEventListener("click", () => section.classList.add("active"));
+      overlay.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
+      closeBtn.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
+
 // for the whole page
 
 const sr = ScrollReveal ({
